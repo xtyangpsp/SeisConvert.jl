06972e1bdc66681df7b3e4fbaeb611d5496c727b
xtyangpsp <stcyang@gmail.com>
2019-07-04 01:20:14 -0400

    dev

--- src/corr2seis.jl
+++ src/corr2seis.jl
@@ -6,14 +6,14 @@ function corr2seis(C::CorrData)
     #get the size of CorrData, to determine how many channels needed.
 
     #create an empty SeisData object with the right size.
-    S = SeisData()
+    S = SeisData(size(C.corr,2))
 
     #loop through the CorrData (corr) and pass the header values to SeisData object
 
 
     # in loop, save customzed headers to misc dictionary.
     # save freqmin and freqmax twice:
-    # 1. use real field name. 2. use USER0 and USER1, to be saved to SAC for later use.
+    # 1. use real field names. 2. use USER0 and USER1, to be saved to SAC for later use.
 
     return(S)
 end
